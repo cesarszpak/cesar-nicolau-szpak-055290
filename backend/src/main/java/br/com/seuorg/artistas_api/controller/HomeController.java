a4e38d0d9baa3225fa4eaf5a56713fab20b38357
Cesar Szpak <cesarszpak@gmail.com>
2026-01-28 15:19:41 -0300

    feat: criar projeto base do backend com Spring Boot e tabela artistas

---
+++ backend/src/main/java/br/com/seuorg/artistas_api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.seuorg.artistas_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API Artistas funcionando!";
+    }
+}
